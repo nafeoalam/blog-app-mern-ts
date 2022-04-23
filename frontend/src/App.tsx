0c565f84ed674125f7c5726295de9e8c14422cac
nafeoalam <nafeo.joy@gmail.com>
2022-04-23 19:54:18 +0600

    Routing Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import Home from 'pages/Home'
-import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 const App = () => {
   return (
     <div>
-      <h1>
-        HELLO !
-      </h1>
-      <Home />
+      <h1>Blog App</h1>
+      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+        <Link to="/home">Home</Link> |{' '}
+        <Link to="/blogs">Blog</Link>
+      </nav>
+      <Outlet />
     </div>
   )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Blog from "pages/Blog";
+import Home from "pages/Home";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="blogs" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
