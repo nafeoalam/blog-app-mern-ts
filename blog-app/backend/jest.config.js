40048c449da0930d8115e609813c008885c5fed5
nafeoalam <nafeo.joy@gmail.com>
2022-06-25 21:20:47 +0600

    Jest Added 2

--- blog-app/backend/jest.config.js
+++ blog-app/backend/jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  },
+  moduleDirectories: ['node_modules', 'src']
 };
